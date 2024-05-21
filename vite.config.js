ecad9d2ca637914445dfe622e5a549df251eada7
Gabriel Sales <kleythongabriel@outlook.com>
2024-05-21 15:02:43 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import mkcert from 'vite-plugin-mkcert'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: true
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    mkcert(),
+    VueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
