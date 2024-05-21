020e78da28080bed288ce378d4e09f164925a1f2
Gabriel Sales <kleythongabriel@outlook.com>
2024-05-21 15:30:13 -0300

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <form>
+    <input type="text">
+  </form>
 </template>
