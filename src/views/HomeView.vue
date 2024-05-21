5a4fec582153791e5118108a89797c9b3ab01f92
Gabriel Sales <kleythongabriel@outlook.com>
2024-05-21 20:01:52 -0300

    0.0.0.4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,20 @@
   <main>
     <div class="container">
       <FormValor @enviarValor="handleValor" />
-      <h1>{{ valorCalculado }}</h1>
-      <h2>{{ valorDizimo }}</h2>
+      <table>
+        <tr>
+          <td class="title">Investimento:</td>
+          <td>R${{ resultado.investimento }}</td>
+        </tr>
+        <tr>
+          <td class="title">Custo de Vida:</td>
+          <td>R${{ resultado.custoVida }}</td>
+        </tr>
+        <tr>
+          <td class="title">Estilo de Vida:</td>
+          <td>R${{ resultado.estiloVida }}</td>
+        </tr>
+      </table>
     </div>
   </main>
 </template>
@@ -12,12 +24,26 @@
 import { ref } from 'vue'
 import FormValor from '../components/FormValor.vue'
 
-const valorCalculado = ref(0)
+const resultado = ref({})
 const valorDizimo = ref(0)
 
 function handleValor(valorRecebido) {
-  valorCalculado.value = valorRecebido[0]
+
   valorDizimo.value = valorRecebido[1]
+
+  let orcamento = {
+    valor: valorRecebido[0],
+    dizimo: valorRecebido[1],
+    investimento: porcento(valorRecebido[0], 0.15),
+    custoVida: porcento(valorRecebido[0], 0.50),
+    estiloVida: porcento(valorRecebido[0], 0.35),
+  }
+
+  resultado.value = orcamento
+}
+
+const porcento = (valor, porcentagem) => {
+  return valor * porcentagem
 }
 </script>
 
@@ -29,5 +55,21 @@ main {
   align-items: center;
   justify-content: center;
   flex-grow: 1;
+
+  table {
+    width: 100%;
+    margin-top: 40px;
+
+    .title {
+      width: 50%;
+      font-weight: bold;
+      text-align: right;
+    }
+
+    td {
+      font-size: 28px;
+      padding: 0 2.5px;
+    }
+  }
 }
 </style>
